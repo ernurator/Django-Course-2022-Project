5067bccae49ed9ed07df0230b81ce8b787ad0305
Yernur <isenbaev121102ernur@gmail.com>
2022-05-02 01:05:11 +0600

    changed balance field typr to float

--- core/models/loan.py
+++ core/models/loan.py
@@ -8,8 +8,8 @@ class Loan(models.Model):
     currency = models.CharField(choices=CurrencyEnum.choices, default=CurrencyEnum.KZT,
                                 max_length=CURRENCY_SYMBOL_LENGTH)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='loans', related_query_name='loan')
-    balance = models.PositiveBigIntegerField(default=0)
+    balance = models.FloatField(default=0)
     rate = models.FloatField()
 
     def __str__(self):
-        return f'Loan({self.balance} {self.currency}) for {self.user} with {self.rate}% rate'
+        return f'Loan of {self.user}, {self.rate}% rate [{self.balance} {self.currency}]'
